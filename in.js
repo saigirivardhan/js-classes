201324da40280024c6ae73a05b2d3f63af0c7e07
saigirivardhan <sandakondasaigirivardhan28@gmail.com>
2022-04-21 21:14:15 +0530

    sai

---
+++ in.js
@@ -0,0 +1,27 @@
+console.log("sai hello world");
+console.log(" hello world");
+console.log(" im the web developer")
+
+var number = 2;//variable
+function Square(n){//function definition
+    var result = n * n;
+    return result;//result will be stored
+}
+var newnumber = Square(5);
+
+let a = 3;
+let b = 4;
+
+
+ let add = a + b;//oper 1
+ let mul = a*b;// oper 2  this will be stored last and first out
+ console.log(add)
+ console.log(mul)
+
+
+
+
+
+
+
+
